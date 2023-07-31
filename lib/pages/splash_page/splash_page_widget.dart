35707d53ff49e5c601f68f53d85581c5b3f9f6ab
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-07-31 12:20:39 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/pages/splash_page/splash_page_widget.dart
@@ -0,0 +1,132 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/custom_code/actions/index.dart' as actions;
+import '/flutter_flow/permissions_util.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'splash_page_model.dart';
+export 'splash_page_model.dart';
+
+class SplashPageWidget extends StatefulWidget {
+  const SplashPageWidget({Key? key}) : super(key: key);
+
+  @override
+  _SplashPageWidgetState createState() => _SplashPageWidgetState();
+}
+
+class _SplashPageWidgetState extends State<SplashPageWidget> {
+  late SplashPageModel _model;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => SplashPageModel());
+
+    // On page load action.
+    SchedulerBinding.instance.addPostFrameCallback((_) async {
+      await requestPermission(bluetoothPermission);
+      if (await getPermissionStatus(bluetoothPermission)) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+              'Permission granted',
+              style: TextStyle(
+                color: FlutterFlowTheme.of(context).primaryText,
+              ),
+            ),
+            duration: Duration(milliseconds: 4000),
+            backgroundColor: FlutterFlowTheme.of(context).secondary,
+          ),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+              'Permission denied',
+              style: TextStyle(
+                color: FlutterFlowTheme.of(context).primaryText,
+              ),
+            ),
+            duration: Duration(milliseconds: 4000),
+            backgroundColor: FlutterFlowTheme.of(context).secondary,
+          ),
+        );
+        return;
+      }
+
+      _model.bluetoothEnabled = await actions.isBluetoothEnabled();
+      if (Navigator.of(context).canPop()) {
+        context.pop();
+      }
+      context.pushNamed(
+        'HomePage',
+        queryParameters: {
+          'isBTEnabled': serializeParam(
+            _model.bluetoothEnabled,
+            ParamType.bool,
+          ),
+        }.withoutNulls,
+      );
+    });
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    _model.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).requestFocus(_model.unfocusNode),
+      child: Scaffold(
+        key: scaffoldKey,
+        backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+        appBar: AppBar(
+          backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+          automaticallyImplyLeading: false,
+          title: Text(
+            'Bluetooth Demo',
+            style: FlutterFlowTheme.of(context).titleLarge,
+          ),
+          actions: [],
+          centerTitle: true,
+          elevation: 0.0,
+        ),
+        body: SafeArea(
+          top: true,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  decoration: BoxDecoration(
+                    color: FlutterFlowTheme.of(context).primaryBackground,
+                  ),
+                  child: Align(
+                    alignment: AlignmentDirectional(0.0, 0.0),
+                    child: Text(
+                      'Fetching bluetooth status...',
+                      style: FlutterFlowTheme.of(context).labelMedium,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/empty_devices/empty_devices_widget.dart
@@ -0,0 +1,61 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'empty_devices_model.dart';
+export 'empty_devices_model.dart';
+
+class EmptyDevicesWidget extends StatefulWidget {
+  const EmptyDevicesWidget({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  final String? text;
+
+  @override
+  _EmptyDevicesWidgetState createState() => _EmptyDevicesWidgetState();
+}
+
+class _EmptyDevicesWidgetState extends State<EmptyDevicesWidget> {
+  late EmptyDevicesModel _model;
+
+  @override
+  void setState(VoidCallback callback) {
+    super.setState(callback);
+    _model.onUpdate();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => EmptyDevicesModel());
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    _model.maybeDispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(),
+      child: Align(
+        alignment: AlignmentDirectional(0.0, 0.0),
+        child: Text(
+          widget.text!,
+          style: FlutterFlowTheme.of(context).labelMedium,
+        ),
+      ),
+    );
+  }
+}
